56553266762b3bee1443587ffdc73e7ac8bcfe4f
harrellk34 <kharrell4@huskers.unl.edu>
2023-08-24 13:04:27 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ *Author: Kieran Harrell
+ *Date 2023-8-22
+ *
+ * Hello World
+ *
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+    printf("Hello World!\n");
+    printf("I love the School of Computing!\n");
+
+    return 0;
+}
