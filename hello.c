69712ac7c8a2ff2e08db5bf87338f9777b9f9d64
Globnefario <142929829+Globnefario@users.noreply.github.com>
2023-08-25 18:44:42 -0500

    Update hello.c to include my name

--- hello.c
+++ hello.c
@@ -13,5 +13,9 @@ int main(int argc, char **argv){
     printf("Hello World!\n");
     printf("I love the School of Computing!\n");
 
+    //added by Xander Tilk
+    printf("Hello, this is Xander\n");
+    printf("My major is Computer Science\n");
+    
     return 0;
 }
